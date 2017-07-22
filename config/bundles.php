229ce5e1c85d288913464f3c5f5564829642c99d
Loulier Guillaume <guillaume.loulier@hotmail.fr>
2017-07-22 17:45:08 +0200

    [CORE](Initialisation)[!M] Initialised

---
+++ config/bundles.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
+    'Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle' => ['all' => true],
+    'Doctrine\Bundle\DoctrineBundle\DoctrineBundle' => ['all' => true],
+    'Symfony\Bundle\WebProfilerBundle\WebProfilerBundle' => ['dev' => true, 'test' => true],
+    'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
+];
